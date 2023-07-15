535a1cfec8a0cae04cc781404ae71f944f6736b0
snowcra4h <snowcra5h@icloud.com>
2023-07-15 00:39:12 -0600

    Working .json files

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int main(int argc, char **argv)
 {
+    (void) argc; 
+    (void) (argv);
 
+
+    char *file_name = NULL; 
+    
+    file_name = readline("Enter a filename to parse: ");
+
+    puts(file_name);
+
+    if (file_name != NULL) {
+        free(file_name);
+        file_name = NULL;
+    }
 
     return 0;
 }
